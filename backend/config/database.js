cdd78ca2c9fb09df30ba6d7819ae69b7bc612d05
ilyasmuhlisin <21ilyasmhlsn@gmail.com>
2022-05-27 21:22:05 +0700

    update config

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,7 +5,10 @@
 
 /** Destruct environment variable to get database configuration */
 const {
-  DB_USERNAME = "iczlmeuh", DB_PASSWORD = "sZdDjl91Wa_fzvy7JHBj8H2jb4NeTO_l", DB_HOST = "tiny.db.elephantsql.com", DB_NAME = "iczlmeuh"
+  DB_USERNAME = "voeimhbh",
+  DB_PASSWORD = "IG31GUWKQx0jc5ybHeDQKritoiLMTifi",
+  DB_HOST = "tiny.db.elephantsql.com",
+  DB_NAME = "voeimhbh",
 } = process.env;
 
 module.exports = {
